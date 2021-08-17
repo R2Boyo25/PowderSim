ca1cfde9eb7cb4560018c4e7a22b7782fde24422
R2boyo25 <r2boyo25@gmail.com>
2021-08-17 14:13:14 -0500

    Remove comments

--- drawpix.py
+++ drawpix.py
@@ -1,7 +1,3 @@
-# Stack Overflow:
-#
-# https://stackoverflow.com/questions/10354638/pygame-draw-single-pixel/64571453#64571453
-
 from powder import Powder
 import pygame, random, time, threading
 
@@ -31,7 +27,6 @@ a = 0
 pos = True
 frame = 0
 curp = [{} for i in range(5)]
-#fps = 30
 chunkAmount = 100
 
 def simPix(frame, window, curp, b, wh, frame2):
@@ -43,13 +38,11 @@ def simPix(frame, window, curp, b, wh, frame2):
 
 while run:
 
-    #dt = clock.tick(60)
 
     if frame == 0:
         random.shuffle(b)
         c = [i for i in chunks(b, round(len(b)/ chunkAmount))]
-    '''for i in range(len(curp)):
-        curp[i]={}'''
+
     curp[frame] = {}
     if a > 254: pos = False
     if a == 0: pos = True
@@ -59,25 +52,6 @@ while run:
             run = False
 
     window.fill(0)
-
-    #rect = pygame.Rect(window.get_rect().center, (0, 0)).inflate(*([min(window.get_size())//2]*2))
-    
-    #for x in range(rect.width):
-    #    u = x / (rect.width - 1)
-    #    color = (round(random.random()*255), a, round(random.random()*255))
-    #    print(color)
-    #    for y in range(rect.height):
-    #        window.set_at((rect.left + x, rect.top + y), color) 
-    '''
-    th = []
-    for Frame in c:
-        t = threading.Thread(target = simPix, args=(Frame, window, curp, b, (w, h), frame))
-        th.append(t)
-    for t in th:
-        t.start()
-    for t in th:
-        t.join()'''
-    
     
     for i in c[frame]:
         i.Draw(window, curp[frame])
@@ -88,15 +62,6 @@ while run:
     for FrameGrid in curp:
         for i in FrameGrid.keys():
             window.set_at(i, (255,255,255))#FrameGrid[i])
-
-    """
-    for x in range(rect.width):
-        for y in range(rect.height):
-            u = x / (rect.width - 1)
-            v = y / (rect.height - 1)
-            color = (round(u*255), round(v*255), round((1-u)*(1-v)*255))
-            window.set_at((rect.left + x, rect.top + y), color) 
-    """
 
     pygame.display.flip()
 
@@ -109,10 +74,7 @@ while run:
         dt_sum = sum(dt_list)
         if dt_sum > 0:
            pygame.display.set_caption("FPS: " + str(round(len(dt_list) / sum(dt_list) * 1000)) + ", TPS: " + str(round(len(dt_list) / sum(dt_list) * 1000) / chunkAmount))
-           #fps = round(len(dt_list) / sum(dt_list) * 1000)
         
-
-    #time.sleep(0.01)
     frame += 1
     if frame == 5:
         frame = 0
